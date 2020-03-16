05b3a826a720c6951e5b9c3cf425e3b22a41330d
skempfer <shana.kempfer88@gmail.com>
2020-03-16 16:50:06 -0300

    Arquivos funcionando e testados com run start

--- semana5/futuretube/src/App.js
+++ semana5/futuretube/src/App.js
@@ -11,7 +11,7 @@ function App() {
       </header>
       <main>
           <nav>
-            <input type="checkbox" id="check">
+            <input type="checkbox" id="check"></input>
             <label for="check"></label>
             <ul>
                 <li><a href="index.html">INÍCIO</a></li>
@@ -22,21 +22,21 @@ function App() {
           </nav>  
           <article>
             <a href="">
-                <img src={require("../src/video1.png")} alt="imagem de cruz celta em fundo ecuro">Bela Irlanda</a></img>
+                <img src={require("../src/video1.png")} alt="imagem de cruz celta em fundo ecuro"></img>Bela Irlanda</a>
             <a href="">
-                <img src={require("../src/video2.png")} alt="imagem de plaquetas sanguínias">Plaquetas sanguínias</a></img>
+                <img src={require("../src/video2.png")} alt="imagem de plaquetas sanguínias"></img>Plaquetas sanguínias</a>
             <a href="">
-                <img src={require("../src/video3.png")} alt="imagem de pintura abstrata colorida">Pintura abstrata</a></img>
+                <img src={require("../src/video3.png")} alt="imagem de pintura abstrata colorida"></img>Pintura abstrata</a>
             <a href="">
-                <img src={require("../src/video4.png")} alt="imagem do mar e areia da praia vistos de cima">Mar e areia</a></img>
+                <img src={require("../src/video4.png")} alt="imagem do mar e areia da praia vistos de cima"></img>Mar e areia</a>
             <a href="">
-                <img src={require("../src/video5.png")} alt="imagem de um campo, com arvores ao fundo e neblina">Campo no outono</a></img>
+                <img src={require("../src/video5.png")} alt="imagem de um campo, com arvores ao fundo e neblina"></img>Campo no outono</a>
             <a href="">
-                <img src={require("../src/video6.png")} alt="imagem de lebre marron em campo verde">Lebre européia</a></img>
+                <img src={require("../src/video6.png")} alt="imagem de lebre marron em campo verde"></img>Lebre européia</a>
             <a href="">
-                <img src={require("../src/video7.png")} alt="imagem do universo, uma estrela nebulosa em tons de roxo">O universo</a></img>
+                <img src={require("../src/video7.png")} alt="imagem do universo, uma estrela nebulosa em tons de roxo"></img>O universo</a>
             <a href="">
-                <img src={require("../src/video8.png")} alt="imagem de foguete pronto para ser lançado ao espaço">Foguete lançado ao espaço</a></img>
+                <img src={require("../src/video8.png")} alt="imagem de foguete pronto para ser lançado ao espaço"></img>Foguete lançado ao espaço</a>
           </article>
         </main>
         <footer>Desenvolvido por SKempfer</footer>
