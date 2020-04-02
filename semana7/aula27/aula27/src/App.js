61780722c1498aa3e0b7930b5417c4f576469899
skempfer <shana.kempfer88@gmail.com>
2020-04-02 20:54:16 -0300

    Filtro pelo número de participantes ok e estilização

--- semana7/aula27/aula27/src/App.js
+++ semana7/aula27/aula27/src/App.js
@@ -13,11 +13,21 @@ const AppContainer = styled.div`
   justify-content: center;
   font-size: calc(10px + 1vmin);
   color: white;
+  
+`;
+
+const Container = styled.div`
+  border: 2px white solid;
+  border-radius: 20px;
+  padding: 60px;
+  background-color: #2F4F4F;
+
 `;
 
 const StiledSelect = styled.select`
   height: 30px;
   width: 90px;
+  font-size: 15px;
 `;
 
 const ButtonSorted = styled.button`
@@ -30,7 +40,7 @@ const ButtonSorted = styled.button`
 
 const ActivityContainer = styled.div`
   border: 2px white solid;
-  padding: 30px;
+  padding: 20px;
   margin: 20px;
   border-radius: 10px;
   background-color: lightblue;
@@ -45,82 +55,78 @@ class App extends React.Component {
       numberOfPeople: "",
       sortedActivity: undefined
     };
-  }
+  };
 
   componentDidMount(){
-    this.getActivity();
-  }
-
-  getActivity = () =>{
-    axios.get('http://www.boredapi.com/api/activity/')
-    .then((response) => {
-      console.log(response.data);
-      this.setState({ sortedActivity: response.data});
-    });
-    
+    this.getActivityUsingNumberOfPeople();
   };
 
   changeNumberOfPeople = (event) => {
+    if(event.target.value !== ""){
     this.setState({numberOfPeople: event.target.value});
-    console.log("sortedActivity trocado", event.target.value)
-  }
-
-  onSelectparticipant = (event) => {
-    
-        
+    console.log("mudou o numberOfPeople: ", event.target.value)
+    }else{
+      this.setState({numberOfPeople: ""});
+    }
   };
-  // alteraMin = novoFilterMin => {
-  //   if (novoFilterMin !== "") {
-  //     this.setState({
-  //       filterMin: novoFilterMin
-  //     })
-  //   } else {
-  //     this.setState({
-  //       filterMin: null
-  //     })
-  //   }
-  // }
+  
+  
 
+  getActivityUsingNumberOfPeople = () => {
+    if(this.state.numberOfPeople !== ""){
+      axios.get(`http://www.boredapi.com/api/activity?participants=${this.state.numberOfPeople}`)
+      .then((response) => {
+        console.log("atividade sorteada com o número de participantes: ", response.data);
+        this.setState({ sortedActivity: response.data});
+      })
+    }else{
+      console.log("Não definido o número de participantes.");
+      // return alert("Escolha o número de participantes. ");
+    }
+  };
 
+  
   render(){
     return (
       <AppContainer>
-        <h1>Atividades para a quarentena</h1>
-        <h3>Escolha o número de participantes:</h3>
-        <p>*Devido as restrições ao contato social, limitamos as atividades
-          ao máximo de dois participantes.</p>
-        <StiledSelect onChange={this.changeNumberOfPeople}>
-          <option value="">Nenhum</option>
-          <option >1</option>
-          <option >2</option>
-          
-        </StiledSelect>
-        <h3>Clique no botão e sorteie uma atividade!</h3>
-        <ButtonSorted onClick={this.getActivity}>Sortear</ButtonSorted>
-        {this.state.sortedActivity && (
-          <ActivityContainer>
-            <p>
-              <strong>Nome: </strong>
-              {this.state.sortedActivity.activity}
-            </p>
-            <p>
-              <strong>Acessibilidade: </strong>
-              {this.state.sortedActivity.accessibility}
-            </p>
-            <p>
-              <strong>Tipo: </strong>
-              {this.state.sortedActivity.type}
-            </p>
-            <p>
-              <strong>Participantes: </strong>
-              {this.state.sortedActivity.participants}
-            </p>
-            <p>
-              <strong>Preço: </strong>
-              {this.state.sortedActivity.price}
-            </p>
-          </ActivityContainer>
-        )}
+        <Container>
+          <h1>Atividades para a quarentena</h1>
+          <h3>Escolha o número de participantes:</h3>
+          <p>*Devido as restrições ao contato social, limitamos as atividades
+            até o máximo de dois participantes.</p>
+          <StiledSelect onChange={this.changeNumberOfPeople}>
+            <option value="">Nenhum</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            
+          </StiledSelect>
+          <h3>Clique no botão e sorteie uma atividade!</h3>
+          <ButtonSorted onClick={this.getActivityUsingNumberOfPeople}>Sortear</ButtonSorted>
+          {this.state.sortedActivity && (
+            <ActivityContainer>
+              <p>
+                <strong>Nome: </strong>
+                {this.state.sortedActivity.activity}
+              </p>
+              <p>
+                <strong>Acessibilidade: </strong>
+                {this.state.sortedActivity.accessibility}
+              </p>
+              <p>
+                <strong>Tipo: </strong>
+                {this.state.sortedActivity.type}
+              </p>
+              <p>
+                <strong>Participantes: </strong>
+                {this.state.sortedActivity.participants}
+              </p>
+              <p>
+                <strong>Preço: </strong>
+                {this.state.sortedActivity.price}
+              </p>
+            </ActivityContainer>
+          )}
+        </Container>
       </AppContainer>
     );
   }
