2a74bc8062003726e868a59feb33471b1d454797
skempfer <shana.kempfer88@gmail.com>
2020-04-19 19:29:04 -0300

    Adicionado actions e reducers

--- semana9/astro-match/src/actions/profiles.js
+++ semana9/astro-match/src/actions/profiles.js
@@ -2,25 +2,45 @@ import axios from 'axios'
 
 // FUNÇÕES SINCRONAS
 
-const setProfileToSwipe = (profile) => ({
-	type: 'SET_PROFILE_TO_SWIPE',
-	payload: {
-		profile,
+export const getAllsMatches = (allsMatches) => {
+	return {
+		type: "GET_ALLS_MATCHES",
+		payload:{
+			allsMatches
+		}
 	}
-})
+}
 
-
-
+export const getNewProfile = (newProfile) => {
+	return{
+		type: "GET_NEW_PROFILE"
+	}
+}
 
 
 //FUNÇÕES ASSINCRONAS
+export const choosePerson = (id,choice) => async (dispatch, getState) => {
+	const body = {
+		id, 
+		choice
+	}
+	const response = await axios.post(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/ana/choose-person`, body)
+	dispatch(getProfileToChoose())
+}
+
 
 export const clearSwipes = () => async (dispatch) => {
-	const reponse = await axios.put('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/ana/clear')
+	const reponse = await axios.delete('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/ana/clear')
+	dispatch(getProfileToChoose())
 }
 
 export const getProfileToChoose = () => async (dispatch, getState) => {
-	const response = await axios.get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:ana/person");
-	console.log(response)
-	dispatch(setProfileToSwipe())
+	const response = await axios.get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/ana/person")
+	
+	dispatch(getNewProfile(response.data.profile))
+}
+
+export const getMatches = () => async (dispatch, getState) => {
+	const response = await axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/ana/matches`)
+	dispatch(getAllsMatches(response.data.matches))
 }
--- semana9/astro-match/src/reducers/profiles.js
+++ semana9/astro-match/src/reducers/profiles.js
@@ -1,7 +1,28 @@
-const initialState = []
+const initialState = {
+  newProfile: {},
+  allsMatches: []
+}
 
 const profiles = (state = initialState, action) => {
-  return state
+  
+  switch (action.type){
+    case "GET_NEW_PROFILE": {
+      return {...state, newProfile: action.payload.newProfile};
+    }
+
+    case "GET_ALLS_MATCHES": {
+      const getMatches = state.allsMatches.filter((matche) =>{
+        if(matche.choise === true){
+          return true;
+        }
+        return false;
+      })
+      return{ ...state, allsMatches: getMatches}
+    }
+
+    default:
+      return state;
+  }
 }
 
 export default profiles
