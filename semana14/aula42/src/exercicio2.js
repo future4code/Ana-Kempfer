2f94fe354af4a7fdf203149e99fe2f2c03f7a54b
skempfer <shana.kempfer88@gmail.com>
2020-06-04 19:18:39 -0300

    terminei exercicio2

--- semana14/aula42/src/exercicio2.js
+++ semana14/aula42/src/exercicio2.js
@@ -1,10 +1,6 @@
 var arr = [22, 47, 50, 1, 4, 500];
-// const isOdd: any (element: number, index: number, array: number){
-//     return (element % !== 0)
-// }
-//Não está aceitando a comparação !==, o código diz que espera um operador TS
-// const arrayOfOdds = [arr].filter(isOdd);
-// console.log("Números Impares: ", arrayOfOdds);
+var oddsArray = arr.filter(function (num) { return num % 2 !== 0; }).length;
+console.log("Números impares: ", oddsArray);
 var total = arr.reduce(function (total, arr) { return total + arr; }, 0);
 console.log("A soma dos números do Array é: ", total);
 console.log("Quantidade de elementos no Array: ", arr.length);
--- semana14/aula42/src/exercicio2.ts
+++ semana14/aula42/src/exercicio2.ts
@@ -1,13 +1,7 @@
 const arr: number[] = [22,47,50,1,4,500];
 
-// const isOdd: any (element: number, index: number, array: number){
-//     return (element % !== 0)
-// }
-//Não está aceitando a comparação !==, o código diz que espera um operador TS
-
-// const arrayOfOdds = [arr].filter(isOdd);
-// console.log("Números Impares: ", arrayOfOdds);
-
+const oddsArray = arr.filter(num => num % 2 !== 0).length;
+console.log("Números impares: ", oddsArray);
 
 const total: number = arr.reduce((total, arr) => total + arr, 0);
 console.log("A soma dos números do Array é: ", total);
