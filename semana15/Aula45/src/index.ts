c7ce7747758032b2e726007746ca28e0df24edbe
skempfer <shana.kempfer88@gmail.com>
2020-06-05 15:07:45 -0300

    criado tipagem do event e função getEvents

---
+++ semana15/Aula45/src/index.ts
@@ -0,0 +1,39 @@
+import * as moment from "moment"
+
+moment.locale("pt-br")
+
+const today = moment()
+today.utcOffset("-0500").format("DD MM YYYY")
+console.log(today.format("DD MM YYYY"))
+
+type event = {
+    name: string, 
+    description: string, 
+    date: string
+}
+
+const arrayOfEvents: any[] = []
+
+function getEvents(): any {
+
+    const taskFile: string = 'taskFile.json';
+      
+        try {
+            
+            const readFile = fs.readFileSync(taskFile)
+            const treatedData: string = readFile.toString()
+            const taskFileToObject: any = JSON.parse(treatedData)
+            console.log(taskFileToObject)
+    
+        } catch (err) {
+    
+            console.log("Erro ao ler arquivo")
+        }
+    }
+
+
+
+
+
+
+
