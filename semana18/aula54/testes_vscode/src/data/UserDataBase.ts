0a705390cc41a773bf15238fe26d14f44fa77136
skempfer <shana.kempfer88@gmail.com>
2020-06-20 20:44:04 -0300

    arquivos iniciais do projeto

---
+++ semana18/aula54/testes_vscode/src/data/UserDataBase.ts
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+import { BaseDataBase } from "./BaseDataBase";
+
+dotenv.config();
+
+export class UserDB extends BaseDataBase {
+
+  private static TABLE_NAME = "userTableName";
+
+  public async createUser(
+    id: string,
+    email: string,
+    password: string
+  ): Promise<void> {
+    await this.getConnection()
+      .insert({
+        id,
+        email,
+        password,
+      })
+      .into(UserDB.TABLE_NAME);
+  } 
+  
+  public async getUserByEmail(email: string): Promise<any> {
+    const result = await this.getConnection()
+      .select("*")
+      .from(UserDB.TABLE_NAME)
+      .where({ email });
+    return result[0];
+  }
+
+  public async getUserById(id: string): Promise<any> {
+    const result = await this.getConnection()
+      .select("*")
+      .from(UserDB.TABLE_NAME)
+      .where({ id });
+    return result[0];
+  }
+}
