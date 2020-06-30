c525fa6b56cc67a64700ca506fb3459001aa2769
skempfer <shana.kempfer88@gmail.com>
2020-06-30 18:18:03 -0300

    exercício 3

--- semana19/Aula62/tests_vscode_aula62/src/UserDataBase.ts
+++ semana19/Aula62/tests_vscode_aula62/src/UserDataBase.ts
@@ -14,14 +14,18 @@ export const validateCharacter = (input: Character): boolean => {
   return true;
 };
 
-export const performAttack = (attacker: Character, defender: Character) => {
-  if (!validateCharacter(attacker) || !validateCharacter(defender)) {
-    throw new Error("Invalid character");
-  }
+export const performAttack = (
+  attacker: Character,
+  defender: Character,
+  validator: (input: Character) => boolean
+  ) => {
+    if (!validator(attacker) || !validator(defender)) {
+      throw new Error("Invalid character");
+    }
 
-  if (attacker.strength > defender.defense) {
-    defender.life -= attacker.strength - defender.defense;
-  }
+    if (attacker.strength > defender.defense) {
+      defender.life -= attacker.strength - defender.defense;
+    }
 };
 
 export interface Character {
