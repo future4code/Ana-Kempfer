a60df10c3c19bb40a8c27a103ae182a8b6ab2b7b
skempfer <shana.kempfer88@gmail.com>
2020-06-30 20:55:40 -0300

    exercício 6

--- semana19/Aula62/tests_vscode_aula62/tests/validateCharacter.test.ts
+++ semana19/Aula62/tests_vscode_aula62/tests/validateCharacter.test.ts
@@ -1,5 +1,9 @@
 
-import { validateCharacter } from "../src/UserDataBase"
+import { 
+    validateCharacter, 
+    Character,
+    performAttack
+aq} from "../src/UserDataBase"
 
 
 describe("Testing validateCharacter", () => {
@@ -63,17 +67,70 @@ describe("Testing validateCharacter", () => {
         expect(result).toBe(true);
     });
 
-    test("Creating Mocks", () => {
-        const validatorMock1 = jest.fn(() => {
-            return true
+    // test("Creating Mocks", () => {
+    //     const validatorMock = jest.fn(() => {
+    //         return true
+    //     });       
+
+    // });
+
+    test("Should perform attack", () => {
+        const validatorMock = jest.fn(() => {
+          return true;
         });
-
-        const validatorMock2 = jest.fn(() => {
-			return false
-		});
-
+    
+        const attacker: Character = {
+          name: "Bananinha",
+          life: 1000,
+          defense: 500,
+          strength: 500,
+        };
+    
+        const defender: Character = {
+          name: "Tomatinho",
+          life: 1000,
+          defense: 400,
+          strength: 400,
+        };
+    
+        performAttack(attacker, defender, validatorMock as any);
+    
+        expect(defender.life).toEqual(800);
+        expect(validatorMock).toHaveBeenCalled();
+        expect(validatorMock).toHaveBeenCalledTimes(2);
+        expect(validatorMock).toHaveReturnedTimes(2);
     });
-       
+
+    test("Should invalid character error", () => {
+        expect.assertions(4);
+
+        const validatorMock = jest.fn(() => {
+          return false;
+        });
+    
+        const attacker: Character = {
+            name: "Bananinha",
+            life: 1000,
+            defense: 500,
+            strength: 500,
+        };
+      
+        const defender: Character = {
+            name: "Tomatinho",
+            life: 1000,
+            defense: 400,
+            strength: 400,
+        };
+    
+        try {
+          performAttack(attacker, defender, validatorMock as any);
+        } catch (err) {
+          expect(err.message).toBe("Invalid character");
+          expect(validatorMock).toHaveBeenCalled();
+          expect(validatorMock).toHaveBeenCalledTimes(1);
+          expect(validatorMock).toHaveReturnedTimes(1);
+        }
+    });      
 
 });
 
